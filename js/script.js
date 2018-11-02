c23f0a35113f8c562f8564cc16bc32c592f8f26d
kisumutalent <homiemusa@gmail.com>
2018-11-02 09:18:18 +0300

    [Finish#161671783]create admin dashoard

--- js/script.js
+++ js/script.js
@@ -38,3 +38,21 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent1, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+} 
