0862abf8e7f7df5c06c97c5f60fea2639ab37e2a
kisumutalent <homiemusa@gmail.com>
2018-11-07 10:17:16 +0300

    [Chore #161748684] Add signup test

---
+++ test_user.py
@@ -0,0 +1,43 @@
+import unittest 
+from __init__ import *
+
+from run import app
+import json
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+
+class UserTestCase(unittest.TestCase):
+	
+		# def setUp(self):
+		# self.app = app("");
+		# self.client = self.app.test_client()
+
+		# user = {
+  #   	"firstname": "wagole",
+  #   	"lastname": "musa",
+  #   	"username": "refuge",
+		# 	"phone": "0725689065",
+		# 	"country": "kenya", 
+		# 	"email": "homie@gmail.com",
+		# 	"password": "wise@12",
+		# 	"confirm_password": "wise@12"
+  #   }
+
+
+
+	def test_register_user(self):
+		tester = app.test_client(self)
+		response = tester.post(
+											'/api/v1/auth/signup', data=json.dumps(
+											dict(firstname="wagole", lastname="musa",username="refuge",\
+												phone="0725689065", country="kenya", email="homie@gmail.com", password="wise@12",\
+												confirm_password="wise@12")),
+											content_type="application/json")
+		self.assertEqual(response.status_code, 200)
+
+
+
+if __name__ =='__main__':
+	unittest.main()
