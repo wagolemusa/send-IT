a6435e2d6b4ffea63b917a8c477935ece917b991
kisumutalent <homiemusa@gmail.com>
2018-11-07 12:30:14 +0300

    [Chore #161748684] Create login test, and get all users test

--- test_user.py
+++ test_user.py
@@ -31,6 +31,48 @@ class UserTestCase(unittest.TestCase):
 											content_type="application/json")
 		self.assertEqual(response.status_code, 200)
 
+	def test_login_user(self):
+		""" User Login Tests with invalid credentials """
+		user = {
+			"username": "refuge",
+			"password": "wise@12"
+		}
+		tester = app.test_client(self)
+		response = tester.post(
+											'/api/v1/auth/signin', data=json.dumps(user),
+											content_type="application/json")
+		self.assertEqual(response.status_code, 200)
+
+
+	def test_login_with_invalid_credentials(self):
+		""" User Login Tests with invalid credentials """
+		user1 = {
+			"username": "refuge",
+			"password": "Pass@qee",
+		}
+		tester = app.test_client(self)
+		response = tester.post(
+											'/api/v1/auth/signin', data=json.dumps(user1),
+											content_type="application/json")
+		self.assertEqual(response.status_code, 200)		
+
+
+
+	def test_get_user_details(self):
+		""" Get user datels """
+		user1 = {
+    	"firstname": "wagole",
+    	"lastname": "musa",
+    	"username": "refuge",
+			"phone": "0725689065",
+			"country": "kenya", 
+			"email": "homie@gmail.com",
+		}
+		tester = app.test_client(self)
+		response = tester.get(
+											'/api/v1/users', data=json.dumps(user1),
+											content_type="application/json")
+		self.assertEqual(response.status_code, 200)		
 
 
 if __name__ =='__main__':
