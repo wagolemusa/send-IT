1aae4066e591ddb82de97ab90caae1efb25024fc
kisumutalent <homiemusa@gmail.com>
2018-11-07 11:52:38 +0300

    [Chore #161748684] Modify  signup test

--- test_user.py
+++ test_user.py
@@ -9,31 +9,25 @@ sys.path.insert(0, os.path.abspath(".."))
 
 
 class UserTestCase(unittest.TestCase):
-	
+
 		# def setUp(self):
 		# self.app = app("");
 		# self.client = self.app.test_client()
 
-		# user = {
-  #   	"firstname": "wagole",
-  #   	"lastname": "musa",
-  #   	"username": "refuge",
-		# 	"phone": "0725689065",
-		# 	"country": "kenya", 
-		# 	"email": "homie@gmail.com",
-		# 	"password": "wise@12",
-		# 	"confirm_password": "wise@12"
-  #   }
-
-
-
 	def test_register_user(self):
+		user = {
+    	"firstname": "wagole",
+    	"lastname": "musa",
+    	"username": "refuge",
+			"phone": "0725689065",
+			"country": "kenya", 
+			"email": "homie@gmail.com",
+			"password": "wise@12",
+			"confirm_password": "wise@12"
+    }
 		tester = app.test_client(self)
 		response = tester.post(
-											'/api/v1/auth/signup', data=json.dumps(
-											dict(firstname="wagole", lastname="musa",username="refuge",\
-												phone="0725689065", country="kenya", email="homie@gmail.com", password="wise@12",\
-												confirm_password="wise@12")),
+											'/api/v1/auth/signup', data=json.dumps(user),
 											content_type="application/json")
 		self.assertEqual(response.status_code, 200)
 
