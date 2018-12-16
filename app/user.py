51d7f45082ce0cb17bcaea2ff2a6932d2625cf26
kisumutalent <homiemusa@gmail.com>
2018-12-16 13:26:22 +0300

    Modify user endpoint

--- app/user.py
+++ app/user.py
@@ -8,8 +8,9 @@ import base64
 from flask import Flask,jsonify,request, make_response
 from flask_restful import Resource
 # from flask_jwt_extended import create_access_token
+
 from flask_jwt_extended import (create_access_token, jwt_required, 
-                                get_jwt_identity, get_current_user, get_raw_jwt)
+                                get_jwt_identity, get_current_user, get_raw_jwt, JWTManager)
 
 from models.user_model import Usermodel
 
