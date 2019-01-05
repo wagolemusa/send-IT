9ccd2546b9ddd23dad3f40607f52f5a2782d0000
kisumutalent <homiemusa@gmail.com>
2019-01-05 04:24:40 +0300

    Modify admin file

--- app/admin.py
+++ app/admin.py
@@ -213,7 +213,6 @@ class PostPrice(Resource):
 	""" Class and Method post price form location """
 	@jwt_required
 	def post(self):
-		connection.commit()
 		current_user = get_jwt_identity()
 		U = Users().get_user_role()
 		if current_user != U:
