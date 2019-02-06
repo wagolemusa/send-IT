57f691ae24e42a035a045720e8512165ff9b1fc5
kisumutalent <homiemusa@gmail.com>
2019-02-06 17:23:22 +0300

    Retrive bookings by ID

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ def create_app(config_name):
 	api.add_resource(SearchDates, '/v2/search/date')
 	api.add_resource(BookingNumber, '/v2/booking/numbers')
 	api.add_resource(ParcelNumber, '/v2/parcel/numbers')
-	api.add_resource(Booking_By_Id, '/v2/booking/<int:book_id>')
+	api.add_resource(Booking_By_Id, '/admin/v2/booking/<int:book_id>')
 
 	# @app.errorhandler(404)
 	# def not_found(error):
