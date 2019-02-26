3b6322b9002fd15f23314f4785fd0c227cbd30a3
kisumutalent <homiemusa@gmail.com>
2019-02-26 12:33:47 +0300

    Changes

--- app/order.py
+++ app/order.py
@@ -347,7 +347,7 @@ class Mpesa(Resource):
 
 class Callback(Resource):
 	def post(self):
-		# results = request.get_json()
+		requests = request.get_json()
 		print (requests)
 
 		data = json.dumps(requests)
