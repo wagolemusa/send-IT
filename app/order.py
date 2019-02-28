71ec6783093e983a65f22361af5d4c72f1d13d99
kisumutalent <homiemusa@gmail.com>
2019-03-01 00:22:55 +0300

    Modifies callback url

--- app/order.py
+++ app/order.py
@@ -418,7 +418,17 @@ class Callback(Resource):
 
 		status = pay()
 		print(status)
+		data = request.get_json(force=True)
+		status = data['status']
 		curr.execute(""" INSERT INTO payments(status)VALUES(%s)"""(status,))
 		connection.commit()
 
 
+		# status = pay()
+		# print(status)
+		# data = request.get_json(force=True)
+		# status = data['status']
+		# print(status)
+		# curr.execute("""UPDATE payments SET status=%s WHERE status =%s""", (status,))
+		# curr.execute("""INSERT INTO payments(status)VALUES(%s)"""(status,))
+		# connection.commit()
