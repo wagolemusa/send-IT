d63580202ac6fc49a7e55de25d1980ab95d04cef
kisumutalent <homiemusa@gmail.com>
2019-03-26 10:03:23 +0300

    Solve SSL

--- app/order.py
+++ app/order.py
@@ -377,9 +377,9 @@ class Mpesa(Resource):
 		consumer_key = "TDWYCw9ChsdHr7QdfcXUS1ddp8gchOC6"
 		consumer_secret = "BdYN5qcwGQvJnMGF"
 
-		api_URL = ("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials") #AUTH URL
+		api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" #AUTH URL
 
-		r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+		r = requests.get('api_URL', verify=False  auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
 		data = r.json()
 		access_token = "Bearer" + ' ' + data['access_token']
