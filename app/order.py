71f28d89c431856842cc2aea04b28ff56ae1a35a
kisumutalent <homiemusa@gmail.com>
2019-09-02 10:36:09 +0300

    Modify database

--- app/order.py
+++ app/order.py
@@ -17,117 +17,13 @@ import africastalking
 from models.user_model import Usermodel
 
 post_pond = ["postpond"]
-
+ 
 connection = psycopg2.connect(dbname='d92a0rb0j8rphh', user='gaijmyignhvtkw', password='7e0acadc7013645d81437d922b7030782cdee4006cadf7f54501aa291b29d3e6', host='ec2-23-21-65-173.compute-1.amazonaws.com')
 curr = connection.cursor()
 
 class Home(Resource):
 	def get(self):
 		return {"message": "SendIT is one of the popular courier services"}
-
-class CreateParcel(Resource):
-	""" Class for create parcel methods """
-	@jwt_required
-	def post(self):
-		data = request.get_json(force=True)
-		title = data['title']
-		pickup = data['pickup']
-		rec_id = data['rec_id']
-		rec_phone = data['rec_phone']
-		rec_name  = data['rec_name']
-		destination = data['destination']
-		weight = data['weight']
-		cash = data['cash']
-		phone = data['phone']
-
-		if title.strip() == '' or pickup.strip() =='' or destination.strip() =='':
-			return jsonify({"message":"Feilds  cannot be blank"})
-		# elif type(rec_id) != int or type(rec_phone) != int or type(weight) != int:
-			# return jsonify({"meassge":"ID, Phone and weight master be numbers"})
-
-		current_user = get_jwt_identity()
-		username = current_user
-		# try:
-		curr.execute(""" INSERT INTO orders(title, username, pickup,rec_id, rec_phone, rec_name, destination, weight, cash, phone)
-																	VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",\
-																		(title, username, pickup, rec_id, rec_phone,rec_name, destination, weight, cash, phone))
-		connection.commit()
-		# return jsonify({"message": 'Successfuly Created an Order'})
-		# except:
-		# 	connection.rollback()
-		# 	return {"message": "Failed to post location"}
-		
-		# Lipa na mpesa Functionality 
-		consumer_key = "TDWYCw9ChsdHr7QdfcXUS1ddp8gchOC6"
-		consumer_secret = "BdYN5qcwGQvJnMGF"
-
-		api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" #AUTH URL
-
-		r = requests.get('api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret)', verify=False)
-
-		data = r.json()
-		access_token = "Bearer" + ' ' + data['access_token']
-
-		#GETTING THE PASSWORD
-		timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
-		passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
-		business_short_code = "174379"
-		data = business_short_code + passkey + timestamp
-		encoded = base64.b64encode(data.encode())
-		password = encoded.decode('utf-8')
-
-
-		# BODY OR PAYLOAD
-		payload = {
-		    "BusinessShortCode": business_short_code,
-		    "Password": password,
-		    "Timestamp": timestamp,
-		    "TransactionType": "CustomerPayBillOnline",
-		    "Amount": cash,
-		    "PartyA": phone,
-		    "PartyB": business_short_code,
-		    "PhoneNumber": phone,
-		    "CallBackURL": "https://senditparcel.herokuapp.com/api/v2/parcel/callbackurl",
-		    "AccountReference": "account",
-		    "TransactionDesc": "account"
-		}
-
-		#POPULAING THE HTTP HEADER
-		headers = {
-		    "Authorization": access_token,
-		    "Content-Type": "application/json"
-		}
-
-		url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest" #C2B URL
-
-		response = requests.post(url, json=payload, headers=headers)
-		print (response.text)
-		return jsonify({"message": 'Thanks for paying'})
-
-
-class ParcelCallbackUrl(Resource):
-	""" Class And Methods creates Callback url for sending parcel"""
-	def post(self):
-		requests = request.get_json()
-		data = json.dumps(requests)
-		json_da = requests.get('Body')
-		resultcode = json_da['stkCallback']['ResultCode']
-
-		def pay():
-			if resultcode == 0:
-				return "Paid"
-			elif resultcode == 1:
-				return "Faild"
-			else:
-				return "Badrequest"
-
-		payments = pay()
-		print(payments)
-
-		curr.execute("""UPDATE orders SET payments=%s WHERE payments = 'NotPaid' """,(payments,))
-		connection.commit()
-
-
 
 	@jwt_required
 	def get(self):
@@ -465,7 +361,7 @@ class Mpesa(Resource):
 		response = requests.post(url, json=payload, headers=headers)
 
 		print (response.text)
-		return {"message": 'Thanks for paying'}
+		return {"message": 'Wait Response on Your phone'}
 
 
 	@jwt_required
@@ -497,9 +393,10 @@ class Mpesa(Resource):
 		return {"book": book_list}
 
 class PaymentId(Resource):
+
 	""" 
 	Methods Queries all Payments 
-
+	
 	"""
 	@jwt_required
 	def get(self, payment_id):
