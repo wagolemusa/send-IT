b789ae05c3351221ffe397660fe8aaaa79b64782
kisumutalent <homiemusa@gmail.com>
2019-10-11 12:47:18 +0300

    Add print cash endpoints

--- app/desk_admin.py
+++ app/desk_admin.py
@@ -15,8 +15,8 @@ import africastalking
 
 types_status = ["delivered", "cancled"]
 
-# connection = psycopg2.connect(dbname='d92a0rb0j8rphh', user='gaijmyignhvtkw', password='7e0acadc7013645d81437d922b7030782cdee4006cadf7f54501aa291b29d3e6', host='ec2-23-21-65-173.compute-1.amazonaws.com')
-# curr = connection.cursor()
+connection = psycopg2.connect(dbname='d92a0rb0j8rphh', user='gaijmyignhvtkw', password='7e0acadc7013645d81437d922b7030782cdee4006cadf7f54501aa291b29d3e6', host='ec2-23-21-65-173.compute-1.amazonaws.com')
+curr = connection.cursor()
 
 class Deskbooking(Resource):
 	@jwt_required
