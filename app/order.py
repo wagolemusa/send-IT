f3f3abc5a55b5cfa1f82392acee347f4b6558fc6
kisumutalent <homiemusa@gmail.com>
2020-06-22 10:07:33 +0300

    solve too many connections

--- app/order.py
+++ app/order.py
@@ -19,10 +19,10 @@ from database import Database
 
 post_pond = ["postpond"]
  
-# connOrder = psycopg2.connect(dbname='d92a0rb0j8rphh', user='gaijmyignhvtkw', password='7e0acadc7013645d81437d922b7030782cdee4006cadf7f54501aa291b29d3e6', host='ec2-23-21-65-173.compute-1.amazonaws.com')
-# currBook = connOrder.cursor()
+connection = psycopg2.connect(dbname='d92a0rb0j8rphh', user='gaijmyignhvtkw', password='7e0acadc7013645d81437d922b7030782cdee4006cadf7f54501aa291b29d3e6', host='ec2-23-21-65-173.compute-1.amazonaws.com')
+curr = connection.cursor()
 
-db = Database()
+# db = Database()
 
 class Home(Resource):
 	def get(self):
